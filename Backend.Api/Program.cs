25986e1b3192dbc6b97253ece3783d6a2ea64664
Wesley Barbosa <wesley.barbosa.developer@gmail.com>
2024-08-23 19:43:33 -0300

    feat: add package Swashbuckle.AspNetCore

--- Backend.Api/Program.cs
+++ Backend.Api/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseHttpsRedirection();
+
+app.Run();
